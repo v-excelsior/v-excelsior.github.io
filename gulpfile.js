c67b16141a85d84679c1ce0d165612afea0f11bb
vakyla98 <vakyla98@ukr.net>
2020-05-24 04:07:35 +0300

    New markup for portfolio

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create()
+
+gulp.task('sass', function () {
+    return gulp.src('./*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream());
+});
+
+
+gulp.task('watchAll', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+        }
+    });
+    gulp.watch('./*.scss', gulp.series('sass'));
+    gulp.watch('./*.html').on('change', browserSync.reload);
+});
+
+gulp.task("default", gulp.series(gulp.parallel("watchAll")));
